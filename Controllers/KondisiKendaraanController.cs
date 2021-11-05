d6ca0a362c01735308795c21a02839ebc4f750c2
ridwannurfaizin12 <ridwannurfaizin12>
2021-11-05 21:52:15 +0700

    Add project files.

---
+++ Controllers/KondisiKendaraanController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentalKendaraan.Models;
+
+namespace RentalKendaraan.Controllers
+{
+    public class KondisiKendaraanController : Controller
+    {
+        private readonly RentalKendaraanContext _context;
+
+        public KondisiKendaraanController(RentalKendaraanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: KondisiKendaraan
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.KondisiKendaraans.ToListAsync());
+        }
+
+        // GET: KondisiKendaraan/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kondisiKendaraan = await _context.KondisiKendaraans
+                .FirstOrDefaultAsync(m => m.IdKondisi == id);
+            if (kondisiKendaraan == null)
+            {
+                return NotFound();
+            }
+
+            return View(kondisiKendaraan);
+        }
+
+        // GET: KondisiKendaraan/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: KondisiKendaraan/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdKondisi,NamaKondisi")] KondisiKendaraan kondisiKendaraan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(kondisiKendaraan);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kondisiKendaraan);
+        }
+
+        // GET: KondisiKendaraan/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kondisiKendaraan = await _context.KondisiKendaraans.FindAsync(id);
+            if (kondisiKendaraan == null)
+            {
+                return NotFound();
+            }
+            return View(kondisiKendaraan);
+        }
+
+        // POST: KondisiKendaraan/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdKondisi,NamaKondisi")] KondisiKendaraan kondisiKendaraan)
+        {
+            if (id != kondisiKendaraan.IdKondisi)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(kondisiKendaraan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KondisiKendaraanExists(kondisiKendaraan.IdKondisi))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(kondisiKendaraan);
+        }
+
+        // GET: KondisiKendaraan/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kondisiKendaraan = await _context.KondisiKendaraans
+                .FirstOrDefaultAsync(m => m.IdKondisi == id);
+            if (kondisiKendaraan == null)
+            {
+                return NotFound();
+            }
+
+            return View(kondisiKendaraan);
+        }
+
+        // POST: KondisiKendaraan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var kondisiKendaraan = await _context.KondisiKendaraans.FindAsync(id);
+            _context.KondisiKendaraans.Remove(kondisiKendaraan);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KondisiKendaraanExists(int id)
+        {
+            return _context.KondisiKendaraans.Any(e => e.IdKondisi == id);
+        }
+    }
+}
